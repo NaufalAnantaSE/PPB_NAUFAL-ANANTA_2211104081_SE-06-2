6e409fff8bb31b05dc92383535625b1a9c22ae15
NaufalAnanta <115007513+NaufalAnantaSE@users.noreply.github.com>
2024-10-02 17:43:42 +0700

    Update Variable.dart

--- 03_Pengenalan_Dart/Guided/Variable.dart
+++ 03_Pengenalan_Dart/Guided/Variable.dart
@@ -1,1 +1,15 @@
+void main() {
+  var name = "yanto";
+  var age = "22";
+  print("name: $name, usia: $age");
+    
 
+  
+ String firstName, lastName; // Tipe data String
+ firstName = "nopal";
+ lastName = "ananta";
+ final cuaca = "Cerah";
+
+ print("nama: $firstName, $lastName" );
+
+}
