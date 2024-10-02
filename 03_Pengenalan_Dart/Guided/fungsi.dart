0a9b07b8d700250b81dbffdc59bfac00a76f296b
NaufalAnanta <115007513+NaufalAnantaSE@users.noreply.github.com>
2024-10-02 17:53:48 +0700

    Create fungsi.dart

---
+++ 03_Pengenalan_Dart/Guided/fungsi.dart
@@ -0,0 +1,15 @@
+void cetakPesan(String pesan) {
+ print(pesan); 
+}//end cetaPesan
+
+int perkalian(int a, int b) {
+ return a * b;
+}//end int
+
+void main() {
+  int hasil = perkalian(5, 10);
+  print("Hasil perkalian: $hasil");
+
+  cetakPesan("Hello World");
+
+}//End main
