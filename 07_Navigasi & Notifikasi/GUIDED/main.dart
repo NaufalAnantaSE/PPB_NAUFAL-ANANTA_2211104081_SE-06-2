bb3e297adbe6d8cab1443d010a691aa3af59f1fa
NaufalAnanta <115007513+NaufalAnantaSE@users.noreply.github.com>
2024-11-06 01:52:36 +0700

    Add files via upload

--- 07_Navigasi & Notifikasi/GUIDED/main.dart
+++ 07_Navigasi & Notifikasi/GUIDED/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'pages/mypage.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '2211104081',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+      ),
+      home:
+        Mypage(),
+    );
+  }
+}
